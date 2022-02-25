ae4769ccae7a48897bea363503f60543b281eee1
Dmitry Kovalyov <laykdimon278@gmail.com>
2022-02-25 18:32:42 +0200

    first commit

---
+++ Core 3.1/Controllers/AccountController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SLE_System.Models;
+
+namespace Core_3._1.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                              SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var newEmail = Guid.NewGuid().ToString() + "@aliases.online";
+                    //var newEmail = "833285df-e60d-45ab-8272-b94bb21ba430@aliases.online";
+                    Job.StartJob(newEmail, model.Password, model.Email);
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+
+            //var branch = new Branch
+            //{
+            //    branchName = "Regie",
+            //    address = "Naval"
+
+            //};
+            //branchContext.Branch.Add(branch);
+            //branchContext.SaveChanges();
+
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, user.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    //Job.StartJob("833285df-e60d-45ab-8272-b94bb21ba430@aliases.online", "*8HXye85sMe$!P4", "laykdimon278@gmail.com");
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(user);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Core-3.1-master/Core 3.1/Job.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using EAGetMail;
+using EASendMail;
+
+namespace Core_3._1
+{
+    public class Job
+    {
+        const string AdminEmail = "layk.taviskaron3@gmail.com";
+        const string AdminPwd = "moWTfplRf8x0";
+        const string ServerAddress = "imap.gmail.com";
+        const string RealAddress = "layk.taviskaron@gmail.com";
+
+        // Generate an unqiue email file name based on date time
+        static string _generateFileName(int sequence)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            return string.Format("{0}-{1:000}-{2:000}.eml",
+                currentDateTime.ToString("yyyyMMddHHmmss", new CultureInfo("en-US")),
+                currentDateTime.Millisecond,
+                sequence);
+        }
+
+
+        public static void StartJob(string inputEmail, string inputPassword)
+        {
+            try
+            {
+                // Create a folder named "inbox" under current directory
+                // to save the email retrieved.
+                string localInbox = string.Format("{0}\\inbox", Directory.GetCurrentDirectory());
+                // If the folder is not existed, create it.
+                if (!Directory.Exists(localInbox))
+                {
+                    Directory.CreateDirectory(localInbox);
+                }
+
+                // Gmail IMAP4 server is "imap.gmail.com"
+                MailServer oServer = new MailServer(ServerAddress,
+                                inputEmail ?? AdminEmail,
+                                inputPassword ?? AdminPwd,
+                                EAGetMail.ServerProtocol.Imap4);
+
+                // Enable SSL connection.
+                oServer.SSLConnection = true;
+
+                // Set 993 SSL port
+                oServer.Port = 993;
+
+                MailClient oClient = new MailClient("TryIt");
+                oClient.Connect(oServer);
+
+                var sentDate = DateTime.Now.AddDays(-40);
+                while (true)
+                {
+                    MailInfo[] infos = oClient.GetMailInfos()
+                        .Where(x => !x.Read)
+                        .ToArray();
+
+                    Console.WriteLine("Total {0} email(s)\r\n", infos.Length);
+
+                    for (int i = 0; i < infos.Length; i++)
+                    {
+                        MailInfo info = infos[i];
+                        Console.WriteLine("Index: {0}; Size: {1}; UIDL: {2}",
+                            info.Index, info.Size, info.UIDL);
+
+                        // Receive email from IMAP4 server
+                        Mail oMail = oClient.GetMail(info);
+                        if (oMail.SentDate <= sentDate)
+                        {
+                            continue; // this email was already synchronized
+                        }
+
+                        sentDate = oMail.SentDate;
+
+                        SendEmail(oMail);
+
+                        Console.WriteLine("From: {0}", oMail.From.ToString());
+                        Console.WriteLine("Subject: {0}\r\n", oMail.Subject);
+
+                        // Generate an unqiue email file name based on date time.
+                        string fileName = _generateFileName(i + 1);
+                        string fullPath = string.Format("{0}\\{1}", localInbox, fileName);
+
+                        // Save email to local disk
+                        oMail.SaveAs(fullPath, true);
+
+                        // Mark email as deleted from IMAP4 server.
+                        oClient.Delete(info);
+                    }
+
+                }
+
+                // Quit and expunge emails marked as deleted from IMAP4 server.
+                oClient.Quit();
+                Console.WriteLine("Completed!");
+            }
+            catch (Exception ep)
+            {
+                Console.WriteLine(ep.Message);
+            }
+        }
+
+        private static void SendEmail(Mail inputMail)
+        {
+            try
+            {
+                SmtpMail oMail = new SmtpMail("TryIt");
+
+                oMail.From = new EASendMail.MailAddress(inputMail.From.Address)
+                {
+                    Additional = inputMail.From.Additional,
+                    Name = inputMail.From.Name
+                };
+
+                // Set recipient email address, please change it to yours
+                oMail.To = RealAddress;
+
+                // Set email subject
+                oMail.Subject = inputMail.Subject;
+                // Set email body
+                oMail.TextBody = inputMail.TextBody;
+
+                // SMTP server address
+                SmtpServer oServer = new SmtpServer(ServerAddress);
+
+                // User and password for ESMTP authentication
+                oServer.User = AdminEmail;
+                oServer.Password = AdminPwd;
+
+                // Most mordern SMTP servers require SSL/TLS connection now.
+                // ConnectTryTLS means if server supports SSL/TLS, SSL/TLS will be used automatically.
+                oServer.ConnectType = SmtpConnectType.ConnectTryTLS;
+
+                // If your SMTP server uses 587 port
+                oServer.Port = 587;
+
+                // If your SMTP server requires SSL/TLS connection on 25/587/465 port
+                // oServer.Port = 25; // 25 or 587 or 465
+                // oServer.ConnectType = SmtpConnectType.ConnectSSLAuto;
+
+                Console.WriteLine("start to send email ...");
+
+                SmtpClient oSmtp = new SmtpClient();
+                oSmtp.SendMail(oServer, oMail);
+
+                Console.WriteLine("email was sent successfully!");
+            }
+            catch (Exception ep)
+            {
+                Console.WriteLine("failed to send email with the following error:");
+                Console.WriteLine(ep.Message);
+            }
+        }
+
+    }
+
+}
